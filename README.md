a7c5117c96c096d8f6f721a8cd9955cafa5b02f4
71e6fd52 <71e6fd52@gmail.com>
2022-03-31 21:48:58 +0800

    export char and state

--- README.md
+++ README.md
@@ -7,5 +7,3 @@ https://coccard.yahvk.moe/
 ## TODO
 
 - [ ] 导出为 hktrpg 格式
-- [ ] 增加属性值导出
-- [ ] 增加状态导出
--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
 import * as XLSX from './xlsx.mjs'
-// import * as XLSX from 'xlsx'
-// import 'xlsx';
+
+let START_ROW: number;
+let END_ROW: number;
+let FIRST_COLUMN: number;
+let SECOND_COLUMN: number;
+
+let DEFAULT: 'ignore' | 'zero' | 'remain';
+let OMEGA: "delete" | "remain";
+
+type HashMap = { [key: string]: number };
+type StateValue = { now: number, max?: number };
 
 function isMergedStart(sheet: XLSX.Sheet, cell: XLSX.CellAddress): boolean {
   return sheet['!merges'].find((i: XLSX.Range) => JSON.stringify(i.s) === JSON.stringify(cell)) != null;
@@ -14,36 +23,15 @@ function getValue(id: string): string {
   return (document.getElementById(id) as HTMLInputElement).value;
 }
 
-async function handleFileAsync(e: Event) {
-  const out = document.getElementById("result") as HTMLElement;
-  const files = (document.getElementById("input_xlsx") as HTMLInputElement).files;
-  if (files == null || files.length == 0) {
-    out.innerText = '未上传文件';
-    return;
-  }
+function getCellByGlobalRef(workbook: XLSX.WorkBook, ref: string): XLSX.CellObject | undefined {
+  let refa = ref.split("!")
+  return workbook.Sheets[refa[0]][refa[1]?.replace(/\$/g, "")];
+}
 
-  const START_ROW = XLSX.utils.decode_row(getValue("start_row"));
-  const END_ROW = XLSX.utils.decode_row(getValue("end_row"));
-  const FIRST_COLUMN = XLSX.utils.decode_col(getValue("first_column"));
-  const SECOND_COLUMN = XLSX.utils.decode_col(getValue("second_column"));
-
-  const DEFAULT = (document.getElementById("ignore_default") as HTMLInputElement).checked ?
-    'ignore' :
-    (document.getElementById("zero_default") as HTMLInputElement).checked ?
-      'zero' :
-      'remain';
-  const OMEGA = (document.getElementById("delete_omega") as HTMLInputElement).checked ?
-    'delete' : 'remain';
-
-  const FORMAT = getValue("format") as 'json' | 'hktrpg'
-
-  out.innerText = '';
-  const file = files[0];
-  const data = await file.arrayBuffer();
-  const workbook = XLSX.read(data);
+function extractSkills(workbook: XLSX.WorkBook): HashMap {
   const sheet = workbook.Sheets['人物卡'];
 
-  const result: { [key: string]: number } = {};
+  const skills: { [key: string]: number } = {};
 
   let j = FIRST_COLUMN;
   for (let i = START_ROW; i <= END_ROW || j == FIRST_COLUMN; ++i) {
@@ -59,13 +47,13 @@ async function handleFileAsync(e: Event) {
       j = SECOND_COLUMN; i = START_ROW;
     }
 
-    if (name == undefined) { continue; }
+    if (name?.w == undefined) { continue; }
     if (init == undefined) { continue; }
     if (value == undefined) { continue; }
     const init_v = init.v as number;
     const value_v = value.v as number;
 
-    let name_v = name.w!;
+    let name_v = name.w;
     if (name_v.endsWith("Ω")) {
       if (OMEGA == 'delete') { continue; }
       name_v = name_v.slice(0, -1)
@@ -76,15 +64,87 @@ async function handleFileAsync(e: Event) {
       switch (DEFAULT) {
         case 'ignore': continue;
         case 'zero':
-          result[name_v] = -1;
+          skills[name_v] = -1;
           break;
         default:
           const _check: never = DEFAULT;
       }
     } else {
-      result[name_v] = value_v;
+      skills[name_v] = value_v;
     }
   }
+
+  return skills;
+}
+
+function extractCharacteristics(workbook: XLSX.WorkBook): HashMap | undefined {
+  if (workbook.Workbook?.Names == undefined) { return undefined; }
+
+  const characteristics: { [key: string]: number } = {};
+  const characteristics_names = ['STR', 'CON', 'SIZ', 'DEX', 'APP', 'EDU', 'INT', 'POW'];
+  for (const name of workbook.Workbook.Names) {
+    if (!characteristics_names.includes(name.Name)) { continue }
+    characteristics[name.Name] = getCellByGlobalRef(workbook, name.Ref)!.v as number;
+  }
+
+  if (characteristics == {}) { return undefined; }
+  return characteristics;
+}
+
+function extractStates(workbook: XLSX.WorkBook): { [key: string]: StateValue } | undefined {
+  if (workbook.Workbook?.Names == undefined) { return undefined; }
+
+  const states: { [key: string]: StateValue } = {};
+  const states_names = ['HP', 'MP', 'SAN'];
+  for (const name of states_names) {
+    let s = workbook.Workbook.Names.find((r) => r.Name == name)
+    let sm = workbook.Workbook.Names.find((r) => r.Name == name + "_MAX")
+    if (s == undefined) { continue; }
+
+    states[name] = { now: getCellByGlobalRef(workbook, s.Ref)!.v as number }
+
+    if (sm != undefined) {
+      states[name].max = getCellByGlobalRef(workbook, sm.Ref)!.v as number
+    }
+
+  }
+
+  if (states == {}) { return undefined; }
+  return states;
+}
+
+async function handleFileAsync(e: Event) {
+  const out = document.getElementById("result") as HTMLElement;
+  const files = (document.getElementById("input_xlsx") as HTMLInputElement).files;
+  if (files == null || files.length == 0) {
+    out.innerText = '未上传文件';
+    return;
+  }
+
+  START_ROW = XLSX.utils.decode_row(getValue("start_row"));
+  END_ROW = XLSX.utils.decode_row(getValue("end_row"));
+  FIRST_COLUMN = XLSX.utils.decode_col(getValue("first_column"));
+  SECOND_COLUMN = XLSX.utils.decode_col(getValue("second_column"));
+
+  DEFAULT = (document.getElementById("ignore_default") as HTMLInputElement).checked ?
+    'ignore' :
+    (document.getElementById("zero_default") as HTMLInputElement).checked ?
+      'zero' :
+      'remain';
+  OMEGA = (document.getElementById("delete_omega") as HTMLInputElement).checked ?
+    'delete' : 'remain';
+
+  const FORMAT = getValue("format") as 'json' | 'hktrpg'
+
+  out.innerText = '';
+  const file = files[0];
+  const data = await file.arrayBuffer();
+  const workbook = XLSX.read(data);
+
+  let result: { skills: HashMap, characteristics?: HashMap, states?: { [key: string]: StateValue } } = { skills: extractSkills(workbook) };
+
+  result.characteristics = extractCharacteristics(workbook);
+  result.states = extractStates(workbook);
 
   switch (FORMAT) {
     case 'json':
